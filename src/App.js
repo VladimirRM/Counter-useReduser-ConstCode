07cfe4360b0e5a011fab32cc380b10d257143622
Vladimir RM <shvdrummer@gmail.com>
2023-01-31 11:17:33 +0200

    create main idea

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
-import React,{useReducer} from 'react';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
 
-function reducer (state,action){
- 
-  switch(action.type){
-    case 'increment':
-      return{
-        counter:state.counter + 1
-      }
+const DECREMENT = "decrement";
+const INCREMENT = "increment";
+
+// const increment = (payload) => ({
+//   type: INCREMENT,
+//   payload,
+// });
+
+const increment = payload =>{
+  return {
+    type:INCREMENT,
+    payload,
+  }
+}
+const decrement = payload =>{
+  return {
+    type:DECREMENT,
+    payload,
+  }
+
+}
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        counter: state.counter + action.payload,
+      };
+    case DECREMENT:
+      return {
+        counter: state.counter - action.payload,
+      };
+      default:
+        return{
+          ...state
+        }
   }
 }
 
 function App() {
-  const [state,dispatch]= useReducer(reducer,{
-    counter: 1
-  })
-
-const handlerClick =()=>{
-    dispatch({
-      type:'increment',
-      payload:1
-
-    })
-}
-
+  const [state, dispatch] = useReducer(reducer, {
+    counter: 1,
+  });
 
   return (
     <div className="App">
-        <button onClick={handlerClick }> You push me {state.counter} times</button>
-        <button onClick={handlerClick }> You push me {state.counter} times</button>
+      <button onClick={() => dispatch(decrement(2))}>-1</button>
+      <button onClick={() => dispatch(decrement(1))}>-2</button>
 
+      <span>{state.counter}</span>
+
+      <button onClick={() => dispatch(increment(1))}>+1</button>
+      <button onClick={() => dispatch(increment(2))}>+2</button>
     </div>
   );
 }
