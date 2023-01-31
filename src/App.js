67a1afd3bf420acf1583e08537507e8db0bd0e53
Vladimir RM <shvdrummer@gmail.com>
2023-01-31 09:31:51 +0200

    add function reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React,{useReducer} from 'react';
 import './App.css';
 
+function reducer (state,action){
+
+}
+
 function App() {
+  const [state,dispatch]= useReducer(reducer,{
+    counter: 1
+  })
 
 
 
-  
+
   return (
     <div className="App">
    
